82f3d1e749a6fde3d95f0c71c1a5eb7b535b9e78
gui1984 <gui1@ymail.com>
2021-05-25 15:03:42 -0300

    first commit
    Commit inicial com  app funcionando

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # FlaskwebGui_exemplo
  Create desktop applications with Flask using flaskwebgui
+
+# About
+Um teste simples feito usando 
+[flaskwebgui](https://github.com/ClimenteA/flaskwebgui)  
+para criar uma aplição desktop flask nativa.
---
+++ main.py
@@ -0,0 +1,24 @@
+#main.py
+
+from flask import Flask  
+from flask import render_template
+from flaskwebgui import FlaskUI # import FlaskUI
+
+
+app = Flask(__name__, template_folder="./")
+ui = FlaskUI(app, width=500, height=500, start_server="flask") # add app and parameters
+
+
+@app.route("/")
+def hello():  
+    return render_template('index.html')
+
+@app.route("/home", methods=['GET'])
+def home(): 
+    return render_template('some_page.html')
+
+
+if __name__ == "__main__":
+    # app.run() for debug
+    ui.run()
+   
